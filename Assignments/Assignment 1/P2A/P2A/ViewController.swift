78cb3398e74aa222ed3608fb0f4afaa0f7603d99
Aaron Wojnowski <aaronwojnowski@gmail.com>
2015-10-05 12:56:23 -0500

    Update the table.

--- Assignments/Assignment 1/P2A/P2A/ViewController.swift
+++ Assignments/Assignment 1/P2A/P2A/ViewController.swift
@@ -8,20 +8,64 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
-
+class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
+    
+    @IBOutlet weak var tableView: NSTableView!
+    private lazy var data: [[String]] = {
+        
+        let inputPath = NSBundle.mainBundle().pathForResource("data", ofType: "txt")
+        let inputData = NSData(contentsOfFile: inputPath!)
+        let inputString: NSString = NSString(data: inputData!, encoding: NSUTF8StringEncoding)!
+        let inputArray = inputString.componentsSeparatedByString("\n")
+        
+        var data = [[String]]()
+        for (var i = 0; i < inputArray.count; i++) {
+            
+            let elementArray = inputArray[i].componentsSeparatedByString(",")
+            
+            var element = [String]()
+            for (var j = 0; j < elementArray.count; j++) {
+                
+                element.append(elementArray[j])
+                
+            }
+            data.append(element)
+            
+        }
+        return data
+        
+    }()
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
 
     override var representedObject: AnyObject? {
         didSet {
-        // Update the view, if already loaded.
+            
         }
     }
-
+    
+    // MARK: NSTableViewDataSource
+    
+    func tableView(tableView: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn?, row: Int) -> AnyObject? {
+        
+        let column = tableView.tableColumns.indexOf(tableColumn!)
+        return self.data[row][column!]
+        
+    }
+    
+    // MARK: NSTableViewDelegate
+    
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        
+        return self.data.count;
+        
+    }
 
 }
 
