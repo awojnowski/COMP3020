3791a49ee91c0c8406677b5420871bdc4a47f29b
Lorenzo Gentile <Lorenzo45@users.noreply.github.com>
2015-10-12 21:45:20 -0500

    Complete part 2b

--- Assignments/Assignment 1/P2B/P2B/AlertWindowController.swift
+++ Assignments/Assignment 1/P2B/P2B/AlertWindowController.swift
@@ -9,12 +9,37 @@
 import Cocoa
 
 class AlertWindowController: NSWindowController {
+    
+    @IBOutlet weak var detailsButton: NSButton!
+    var detailsShowing = false {
+        didSet {
+            updateDetails(true)
+        }
+    }
 
     override func windowDidLoad() {
         
         super.windowDidLoad()
-    
+        updateDetails(false)
         
+    }
+    
+    func updateDetails(animated: Bool) {
+        
+        guard let window = self.window else {
+            print("No window")
+            return
+        }
+        
+        let newTitle = detailsShowing ? "▴Details" : "▾Details"
+        let heightChage: CGFloat = detailsShowing ? 100 : -100
+        
+        detailsButton.title = newTitle
+        
+        var newFrame = window.frame
+        newFrame.size.height += heightChage
+        newFrame.origin.y -= heightChage // So top stays in place (by default it's the bottom)
+        window.setFrame(newFrame, display: true, animate: animated)
         
     }
     
@@ -22,8 +47,19 @@ class AlertWindowController: NSWindowController {
     
     @IBAction func cancel(sender: AnyObject) {
         
-        self.window!.sheetParent!.endSheet(self.window!, returnCode: NSModalResponseCancel)
-        self.window!.orderOut(sender)
+        window?.close()
+        
+    }
+    
+    @IBAction func okPressed(sender: AnyObject) {
+        
+        window?.close()
+        
+    }
+    
+    @IBAction func detailsPressed(sender: AnyObject) {
+        
+        detailsShowing = !detailsShowing
         
     }
 
