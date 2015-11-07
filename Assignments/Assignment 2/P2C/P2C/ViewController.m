600d47a0b382bb0c87958d0e7eeabac1d79f6c79
Aaron Wojnowski <aaronwojnowski@gmail.com>
2015-11-07 08:54:09 -0600

    Almost implement part 3.

--- Assignments/Assignment 2/P2C/P2C/ViewController.m
+++ Assignments/Assignment 2/P2C/P2C/ViewController.m
@@ -8,18 +8,55 @@
 
 #import "ViewController.h"
 
+CGFloat const ViewControllerMinimumTextFieldWidth = 100.0;
+
+@interface ViewController ()
+
+@property (nonatomic, weak) IBOutlet NSScrollView *scrollView;
+@property (nonatomic, weak) IBOutlet NSTextField *textField;
+
+@end
+
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-
-    // Do any additional setup after loading the view.
+-(void)dealloc {
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    
 }
 
-- (void)setRepresentedObject:(id)representedObject {
-    [super setRepresentedObject:representedObject];
+-(void)viewDidLoad {
+    
+    [super viewDidLoad];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(controlTextDidChange:) name:NSControlTextDidChangeNotification object:[self textField]];
 
-    // Update the view, if already loaded.
+}
+
+#pragma mark - Notifications
+
+-(void)controlTextDidChange:(NSNotification *)notification {
+    
+    NSString * const string = [[self textField] stringValue];
+    NSLog(@"Updated string to: %@",string);
+    
+    NSSize size = [string sizeWithAttributes:@{ NSFontAttributeName : [[self textField] font] }];
+    size.width += 8.0; // padding
+    
+    [[[self textField] constraints] enumerateObjectsUsingBlock:^(NSLayoutConstraint * _Nonnull constraint, NSUInteger idx, BOOL * _Nonnull stop) {
+    
+        if ([constraint firstAttribute] == NSLayoutAttributeWidth) {
+            
+            [constraint setConstant:MAX(ViewControllerMinimumTextFieldWidth, size.width)];
+            *stop = YES;
+            
+        }
+        
+    }];
+    
+    NSView * const documentView = [[self scrollView] documentView];
+    [documentView setBounds:NSMakeRect(0, 0, MAX(CGRectGetMaxX([[self textField] frame]) + CGRectGetMinX([[self textField] frame]), CGRectGetWidth([[[self view] window] frame])), CGRectGetHeight([documentView bounds]))];
+    
 }
 
 @end
