723a3da00b9c05faebdb517b5eb6c41af9926842
Aaron Wojnowski <aaronwojnowski@gmail.com>
2015-11-07 09:06:16 -0600

    Add part 3.

---
+++ Assignments/Assignment 2/P3/P3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  P3
+//
+//  Created by Aaron Wojnowski on 2015-11-07.
+//  Copyright © 2015 CS Boys. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ Assignments/Assignment 2/P3/P3/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  P3
+//
+//  Created by Aaron Wojnowski on 2015-11-07.
+//  Copyright © 2015 CS Boys. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+@end
