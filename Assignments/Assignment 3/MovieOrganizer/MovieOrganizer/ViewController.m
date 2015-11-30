73bf1b4d5e987547837908a2e8cb052f28db27cd
Aaron Wojnowski <aaronwojnowski@gmail.com>
2015-11-30 15:30:04 -0600

    Add seed data.

--- Assignments/Assignment 3/MovieOrganizer/MovieOrganizer/ViewController.m
+++ Assignments/Assignment 3/MovieOrganizer/MovieOrganizer/ViewController.m
@@ -11,15 +11,19 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
 
-    // Do any additional setup after loading the view.
+    
+    
 }
 
 - (void)setRepresentedObject:(id)representedObject {
+    
     [super setRepresentedObject:representedObject];
 
-    // Update the view, if already loaded.
+    
+    
 }
 
 @end
